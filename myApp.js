580319a48125026dbf24189e10b08a1814fa241d
unknown <jrmcgee@asu.edu>
2021-04-21 20:20:46 +0900

    Added files

---
+++ myApp.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+app.use('/public', express.static(__dirname + "/public/"));
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
